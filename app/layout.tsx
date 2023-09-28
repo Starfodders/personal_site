c2f8d75094ac8b26df892bd6592838cdba83e913
Michael Deng <michaeldeng55@gmail.com>
2023-09-27 23:55:46 -0400

    trying to fix og

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,15 @@ export const metadata = {
   icons: {
     icon: '/favicon/favicon.ico',
     apple: '/favicon/apple-touch-icon.png'
+  },
+  opengraph: {
+    title: 'Michael Deng',
+    description: 'Software Engineer Portfolio for Michael Deng',
+    image: 'https://michaeldeng.netlify.app/opengraphIMG.png',
+    type: 'website',
+    url: 'https://michaeldeng.netlify.app/',
+    domain: 'michaeldeng.netlify.app',
+    card: 'summary_large_image'
   }
 }
 
@@ -17,16 +26,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <title>{metadata.title}</title>
         <link rel="icon" href={metadata.icons.icon} />
         <link rel="apple-touch-icon" href={metadata.icons.apple} />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content="https://michaeldeng.netlify.app/opengraphIMG.png" />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="michaeldeng.netlify.app" />
-        <meta property="twitter:url" content="https://michaeldeng.netlify.app/" />
-        <meta name="twitter:title" content="Michael Deng" />
-        <meta name="twitter:description" content="Software Engineer Portfolio for Michael Deng" />
-        <meta name="twitter:image" content="https://michaeldeng.netlify.app/opengraphIMG.png" />
+        <meta property="og:title" content={metadata.opengraph.title} />
+        <meta property="og:description" content={metadata.opengraph.description} />
+        <meta property="og:image" content={metadata.opengraph.image} />
+        <meta property="og:type" content={metadata.opengraph.type} />
+        <meta property="og:url" content={metadata.opengraph.url} />
+        <meta name="twitter:card" content={metadata.opengraph.card} />
+        <meta property="twitter:domain" content={metadata.opengraph.domain} />
+        <meta property="twitter:url" content={metadata.opengraph.url} />
+        <meta name="twitter:title" content={metadata.opengraph.title} />
+        <meta name="twitter:description" content={metadata.opengraph.description} />
+        <meta name="twitter:image" content={metadata.opengraph.image} />
       </Head>
       {children}
     </>
