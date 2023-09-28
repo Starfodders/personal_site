22d28bebf9be216a292d99a2ee10c729d2903e01
Michael Deng <michaeldeng55@gmail.com>
2023-09-27 23:44:18 -0400

    trying to fix og

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,28 @@
 import './globals.css'
-import type { Metadata } from 'next'
 import Head from 'next/head';
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Michael Deng',
   description: 'Software Engineer Portfolio for Michael Deng',
   icons: {
     icon: '/favicon/favicon.ico',
     apple: '/favicon/apple-touch-icon.png'
   }
-
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <>
       <Head>
-        <meta property="og:title" content= 'Michael Deng' />
-        <meta property="og:description" content= 'Software Engineer 2023 Portfolio for Michael Deng' />
-        <meta property="og:image" content='/logo.png' /> 
+        <title>{metadata.title}</title>
+        <link rel="icon" href={metadata.icons.icon} />
+        <link rel="apple-touch-icon" href={metadata.icons.apple} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content='https://michaeldeng.netlify.app/logo.png' />
         <meta property="og:type" content="website" />
       </Head>
-      <body>{children}</body>
-    </html>
+      {children}
+    </>
   )
 }
