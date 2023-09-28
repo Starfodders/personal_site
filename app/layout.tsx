b736ceef9f79713e97deaec9958a830cb7b677ce
Michael Deng <michaeldeng55@gmail.com>
2023-09-27 23:37:01 -0400

    fixing opengraph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: 'Michael Deng',
@@ -8,6 +9,7 @@ export const metadata: Metadata = {
     icon: '/favicon/favicon.ico',
     apple: '/favicon/apple-touch-icon.png'
   }
+
 }
 
 export default function RootLayout({
@@ -17,6 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:title" content= 'Michael Deng' />
+        <meta property="og:description" content= 'Software Engineer 2023 Portfolio for Michael Deng' />
+        <meta property="og:image" content='/logo.png' /> 
+        <meta property="og:type" content="website" />
+      </Head>
       <body>{children}</body>
     </html>
   )
