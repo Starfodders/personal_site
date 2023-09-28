a105a68046e3edaef8421e4a1e858419bd03044d
Michael Deng <michaeldeng55@gmail.com>
2023-09-28 00:01:34 -0400

    trying to fix og

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,40 @@
 import './globals.css'
 import Head from 'next/head';
+import { Metadata } from 'next';
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Michael Deng',
   description: 'Software Engineer Portfolio for Michael Deng',
   icons: {
     icon: '/favicon/favicon.ico',
     apple: '/favicon/apple-touch-icon.png'
   },
-  opengraph: {
+  openGraph: {
     title: 'Michael Deng',
     description: 'Software Engineer Portfolio for Michael Deng',
-    image: 'https://michaeldeng.netlify.app/opengraphIMG.png',
+    images: [
+      {
+        url:'https://michaeldeng.netlify.app/opengraphIMG.png',
+        width: 800,
+        height: 600
+      },
+      {
+        url:'https://michaeldeng.netlify.app/opengraphIMG.png',
+        width: 1800,
+        height: 1600
+      }
+    ],
     type: 'website',
+    locale: 'en_US',
     url: 'https://michaeldeng.netlify.app/',
-    domain: 'michaeldeng.netlify.app',
-    card: 'summary_large_image'
+    siteName: 'Michael Deng'
   }
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <Head>
-        <title>{metadata.title}</title>
-        <link rel="icon" href={metadata.icons.icon} />
-        <link rel="apple-touch-icon" href={metadata.icons.apple} />
-        <meta property="og:title" content={metadata.opengraph.title} />
-        <meta property="og:description" content={metadata.opengraph.description} />
-        <meta property="og:image" content={metadata.opengraph.image} />
-        <meta property="og:type" content={metadata.opengraph.type} />
-        <meta property="og:url" content={metadata.opengraph.url} />
-        <meta name="twitter:card" content={metadata.opengraph.card} />
-        <meta property="twitter:domain" content={metadata.opengraph.domain} />
-        <meta property="twitter:url" content={metadata.opengraph.url} />
-        <meta name="twitter:title" content={metadata.opengraph.title} />
-        <meta name="twitter:description" content={metadata.opengraph.description} />
-        <meta name="twitter:image" content={metadata.opengraph.image} />
-      </Head>
+      <body>
       {children}
-    </>
+    </body>
   )
 }
